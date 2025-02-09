ee575d5fa0f9a0a823d63e9db0c6f745ce76b793
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2025-02-10 00:37:27 +0530

    hello

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.openai.home',
+  appName: 'com.openiot.home',
+  webDir: 'dist'
+};
+
+export default config;
